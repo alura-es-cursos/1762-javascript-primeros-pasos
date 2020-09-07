bb8b5e93f25fbf3d2f13d7bfe0f2349d1f5a50f6
Leonardo <ljcl79@gmail.com>
2020-09-07 09:45:12 -0300

    Aula 1

---
+++ 1-HolaMundo.js
@@ -0,0 +1,12 @@
+console.log('Mi primer programa');
+console.log(2+2);
+
+const nombreMadre = "María";
+const nombrePadre = "José";
+const nombreHijo = "Jesús";
+
+const edadMadre = 60;
+
+console.log(nombreMadre);
+console.log("El nombre de mi madre es "+nombreMadre);
+console.log("La edad de mi madre es "+edadMadre);
